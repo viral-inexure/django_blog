83843bf608282f7b9fd65a1b2b5e8d415d4570f3
viral-inexture <viralmalaviya.inexture@gmail.com>
2021-08-17 14:12:07 +0530

    my project

--- django_project/settings.py
+++ django_project/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
