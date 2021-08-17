35f66662c38167aef7db5473e043d0eb8e2027e3
viral-inexture <viralmalaviya.inexture@gmail.com>
2021-08-17 14:02:07 +0530

    my project

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
-    'crispy-forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
