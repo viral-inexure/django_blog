e4395c3d3b790799dd2c2bc1e469fa67322814c1
viral-inexture <viralmalaviya.inexture@gmail.com>
2021-08-19 10:21:31 +0530

    pagination chage 5 to 3 in single page

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 3
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
